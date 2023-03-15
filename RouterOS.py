5aa633cc80b1c39fcf69b2fbbcc04244a141ba4d
Mariusz Brzeski <marianob85@gmail.com>
2023-03-16 00:23:50 +0100

    Modified   .gitignore Modified   .vscode/launch.json Modified   upgrade-routeros.py Added      RouterOS.py

---
+++ RouterOS.py
@@ -0,0 +1,222 @@
+import sys
+import re
+import paramiko
+import time
+import packaging.version
+from multiping import multi_ping
+
+
+RouterOS_regex = re.compile('^ *([^:]*): (.*)')
+MikroTik_version_regex = re.compile('^([^ ]*)')
+
+
+class RouterOSCommand:
+    def __init__(self, message, attributes=None, verbose=None):
+        for attribute in attributes:
+            setattr(self, attribute, None)
+        for line in message:
+            line = line.rstrip('\r\n')
+            if verbose and verbose >= 3:
+                print('... ' + line)
+            m = RouterOS_regex.match(line)
+            if m:
+                key = m.group(1).replace('-', '_')
+                value = m.group(2)
+                if attributes and key in attributes:
+                    setattr(self, key, value)
+                    if verbose and verbose >= 2:
+                        print(f"\t{key}: {value}")
+
+
+class RouterOsUpgrade:
+    def __init__(self, timeout, sshretries, reboot_timeout, username, password, verbose=0):
+        self.timeout = timeout
+        self.sshretries = sshretries
+        self.reboot_timeout = reboot_timeout
+        self.username = username
+        self.password = password
+        self.verbose = verbose
+
+    def connect(self, hostname):
+        SSHClient = paramiko.SSHClient()
+        SSHClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        retries = 0
+        connected = False
+        while not connected:
+            try:
+                SSHClient.connect(hostname, username=self.username, password=self.password, timeout=self.timeout)
+                connected = True
+                break
+            except:
+                if retries > self.sshretries:
+                    break
+                print("SSH connection failed. Retrying.")
+                retries += 1
+                time.sleep(retries)
+
+        if not connected:
+            print("ERROR: SSH connection failed.")
+            SSHClient.close()
+            return None
+        return SSHClient
+
+    def checkForNewVersion(self, sshclient):
+        sshclient.exec_command('/system package update check-for-updates once')
+        time.sleep(3)
+        _, stdout, _ = sshclient.exec_command('/system package update print')
+        mtUpgradeInfo = RouterOSCommand(stdout, attributes=['installed_version', 'latest_version', 'status'], verbose=self.verbose)
+        if mtUpgradeInfo.latest_version:
+            return mtUpgradeInfo
+        return None
+
+    def mapVersion(self, version):
+        m = MikroTik_version_regex.match(version)
+        if m:
+            version = m.group(1)
+            return packaging.version.parse(version)
+        return ""
+
+    def waitForResponse(self, hostname):
+        reboot_time = time.time()
+        timeout = time.time() + self.reboot_timeout
+        while time.time() < timeout:
+            _, no_responses = multi_ping([hostname], timeout=10, retry=2)
+            if not no_responses:
+                return True, time.time() - reboot_time
+            print('\r{:.0f} seconds since reboot...'.format(time.time() - reboot_time))
+        return False
+
+    def makeOSUpgrade(self, hostname):
+        if self.verbose:
+            print("Checking RouterOS version")
+        try:
+            SSHClient = self.connect(hostname)
+            if not SSHClient:
+                return False, None
+
+            stdin, stdout, stderr = SSHClient.exec_command('/system resource print')
+            mtResources = RouterOSCommand(stdout, attributes=['version', 'architecture_name', 'board_name', 'bad_blocks'], verbose=self.verbose)
+
+            if not mtResources.version:
+                print("Failed to get current RouterOS version. Skipping upgrade.")
+                return False, None
+            else:
+                CurVersion = self.mapVersion(mtResources.version)
+
+            if (mtResources.architecture_name == ""):
+                print("Failed to get RouterOS architecture-name. Skipping upgrade.")
+                return False, None
+
+            mtUpgradeInfo = self.checkForNewVersion(SSHClient)
+            if not mtUpgradeInfo:
+                print("Failed to get RouterOS latest version information. Skipping upgrade.")
+                return False, None
+
+            if mtUpgradeInfo.installed_version == mtUpgradeInfo.latest_version:
+                print("RouterOS version already {}".format(mtUpgradeInfo.latest_version))
+                return True, mtUpgradeInfo.latest_version
+
+            print("RouterOS version from {} to {}".format(mtUpgradeInfo.installed_version, mtUpgradeInfo.latest_version))
+            SSHClient.exec_command('/system package update install')
+
+            time.sleep(30)
+            host_up, reboot_time = self.waitForResponse(hostname)
+            if not host_up:
+                print("ERROR: {} has NOT come back online within {} seconds. ".format(hostname, self.reboot_timeout))
+                return False, None
+            if host_up:
+                print('{} is back online after {:.0f} seconds. Checking status'.format(hostname, reboot_time), flush=True)
+                time.sleep(5)  # Wait 5 seconds for the device to fully boot
+
+                SSHClient = self.connect(hostname)
+                if not SSHClient:
+                    return False, None
+
+                stdin, stdout, stderr = SSHClient.exec_command('/system resource print')
+                mtResources = RouterOSCommand(stdout, attributes=['version'], verbose=self.verbose)
+
+                if not mtResources.version:
+                    print("ERROR: Could not confirm RouterOS version.")
+                    return False, None
+
+                CurVersion = self.mapVersion(mtResources.version)
+                LastVersion = packaging.version.parse(mtUpgradeInfo.latest_version)
+                if (CurVersion < LastVersion):
+                    print("ERROR: Upgrade of {} did not occur, current RouterOS version {}".format(hostname, mtResources.version))
+                    return False, mtResources.version
+                else:
+                    print("{} RouterOS successfully upgraded. Version now {}".format(hostname, mtResources.version))
+                    return True, mtResources.version
+        except:
+            if SSHClient: 
+                SSHClient.close()
+                return False, None
+        finally:
+            if SSHClient: 
+                SSHClient.close()
+        return True
+
+    def makeFirmwareUpdate(self, hostname):
+        if self.verbose:
+            print("Checking firmware version")
+        try:
+            SSHClient = self.connect(hostname)
+            if not SSHClient:
+                return False, None
+
+            stdin, stdout, stderr = SSHClient.exec_command('/system routerboard print')
+            mtResources = RouterOSCommand(stdout, attributes=['current_firmware', 'upgrade_firmware'], verbose=self.verbose)
+
+            if not mtResources.current_firmware or not mtResources.upgrade_firmware:
+                print("Failed to get current firmware version. Skipping upgrade.")
+                return False, None
+
+            NewVersion = packaging.version.parse(mtResources.upgrade_firmware)
+            CurVersion = packaging.version.parse(mtResources.current_firmware)
+            if CurVersion >= NewVersion:
+                print("Firmware version already {}".format(mtResources.current_firmware))
+                return True, mtResources.current_firmware
+
+            print("Firmware version from {} to {}".format(mtResources.current_firmware, mtResources.upgrade_firmware))
+            SSHClient.exec_command('/system routerboard upgrade')
+
+            if self.verbose:
+                print("Rebooting in 15 seconds.")
+            time.sleep(15)
+            print("Rebooting {}".format(hostname))
+            SSHClient.exec_command('/system reboot')
+            time.sleep(5)
+            host_up, reboot_time = self.waitForResponse(hostname)
+            if not host_up:
+                print("ERROR: {} has NOT come back online within {} seconds. ".format(hostname, self.reboot_timeout))
+                return False, None
+            if host_up:
+                print('{} is back online after {:.0f} seconds. Checking status'.format(hostname, reboot_time), flush=True)
+                time.sleep(5)  # Wait 5 seconds for the device to fully boot
+
+                SSHClient = self.connect(hostname)
+                if not SSHClient:
+                    return False, None
+
+                stdin, stdout, stderr = SSHClient.exec_command('/system routerboard print')
+                mtResources = RouterOSCommand(stdout, attributes=['current_firmware', 'upgrade_firmware'], verbose=self.verbose)
+
+                if not mtResources.current_firmware or not mtResources.upgrade_firmware:
+                    print("Failed to get current firmware version. Skipping upgrade.")
+                    return False, None
+
+                NewVersion = packaging.version.parse(mtResources.upgrade_firmware)
+                CurVersion = packaging.version.parse(mtResources.current_firmware)
+                if (CurVersion < NewVersion):
+                    print("ERROR: Upgrade of {} Firmware did not occur, current Firmware version {}".format(hostname, mtResources.current_firmware))
+                    return False, mtResources.current_firmware
+                else:
+                    print("{} Firmware successfully upgraded. Version now {}".format(hostname, mtResources.current_firmware))
+                    return True, mtResources.current_firmware
+        except:
+            if SSHClient: 
+                SSHClient.close()
+            return False, None
+        finally:
+            if SSHClient: 
+                SSHClient.close()
--- upgrade-routeros.py
+++ upgrade-routeros.py
@@ -1,299 +1,37 @@
-#!/usr/bin/python3
-
-import sys
-import os
-import shutil
-import re
-import time
-import getpass
-import packaging.version
-from multiping import multi_ping
-import paramiko
-
-
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--timeout',			type=int,	help='SSH timeout in seconds, default: 60')
-parser.add_argument('-s', '--sshstop',   action="store_true",	help='Stop upgrades of further devices if SSH fails on initial connection, default: false')
-parser.add_argument('-R', '--sshretries',		type=int,	help='SSH retries, default: 3')
-parser.add_argument('-r', '--reboot_timeout',			help='Timeout after reboot before upgrade considered failed, default: 180')
-parser.add_argument('-u', '--username',				help='Username for access to RouterOS, default: local username')
-parser.add_argument('-p', '--password',	required=True,				help='password for access to RouterOS, default:')
-parser.add_argument('-v', '--verbose',   action="count",	help='Verbose output')
-parser.add_argument('hosts', metavar='HOST', type=str, nargs='+', help='RouterOS host to upgrade')
-args = parser.parse_args()
+import RouterOS
 
-class bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	WARNING = '\033[103;30m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+# https://github.com/andrewradke/MikroTik-upgrade/blob/master/upgrade-routeros.py
 
+if __name__ == '__main__':
 
-if args.username:
-	username = args.username
-else:
-	username = getpass.getuser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--timeout', type=int, help='SSH timeout in seconds', default=60)
+    parser.add_argument('-R', '--sshretries', type=int, help='SSH retries', default=3)
+    parser.add_argument('-r', '--reboot_timeout', type=int, help='Timeout after reboot before upgrade considered failed', default=180)
+    parser.add_argument('-u', '--username', help='Username for access to RouterOS, default: local username')
+    parser.add_argument('-p', '--password', required=True, help='password for access to RouterOS, default:')
+    parser.add_argument('-v', '--verbose', action="count", help='Verbose output')
+    parser.add_argument('hosts', metavar='HOST', type=str, nargs='+', help='RouterOS host to upgrade')
+    args = parser.parse_args()
 
-if args.timeout:
-	timeout = args.timeout
-else:
-	timeout = 10
+    routerOsUpgrade = RouterOS.RouterOsUpgrade(timeout=args.timeout, sshretries=args.timeout, reboot_timeout=args.reboot_timeout,
+                                               username=args.username, password=args.password, verbose=args.verbose)
 
-if args.sshretries:
-	sshretries = args.sshretries
-else:
-	sshretries = 10
+    upgradeStatus = {}
 
-if args.reboot_timeout:
-	reboot_timeout = int(args.reboot_timeout)
-else:
-	reboot_timeout = 180
+    for hostname in args.hosts:
+        print("\n\n\n*** {} ***".format(hostname))
 
-password = args.password
+        routerOsStatus, routerOsVersion = routerOsUpgrade.makeOSUpgrade(hostname)
+        if routerOsStatus:
+            firmwareStatus, firmwareVersion = routerOsUpgrade.makeFirmwareUpdate(hostname)
+        else:
+            firmwareStatus = False
+            firmwareVersion = None
 
-if args.verbose:
-	print("Verbose output enabled")
-	print("Verbose level {}".format(args.verbose))
-	print("Username: '{}'".format(username))
-	print("Timeout: {} seconds".format(timeout))
+        upgradeStatus[hostname] = (routerOsStatus, routerOsVersion, firmwareStatus, firmwareVersion)
 
-MikroTik_regex = re.compile('^ *([^:]*): (.*)')
-MikroTik_version_regex = re.compile('^([^ ]*)')
-
-for hostname in args.hosts:
-	if sys.stdout.isatty():
-		print(bcolors.BOLD + bcolors.UNDERLINE, end='')
-	print("\n*** {} ***".format(hostname))
-	if sys.stdout.isatty():
-		print(bcolors.ENDC, end='')
-	if args.verbose:
-		print("Checking RouterOS version")
-	version	= ""
-	architecture_name = ""
-	board_name = ""
-	bad_blocks = ""
-
-	SSHClient = paramiko.SSHClient()
-	SSHClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	connected = False
-	retries   = 0
-	while not connected:
-		try:
-			SSHClient.connect(hostname, username=username, password=password, timeout=timeout)
-			connected = True
-			break
-		except:
-			if retries > sshretries:
-				break
-			print(bcolors.WARNING + "SSH connection failed. Retrying." + bcolors.ENDC)
-			retries += 1
-			time.sleep(retries)
-			
-	if not connected:
-		if sys.stdout.isatty():
-			print(bcolors.FAIL, end='')
-		print("ERROR: SSH connection failed.")
-		if args.sshstop:
-			print("Updates to ALL FURTHER devices cancelled!")
-		if sys.stdout.isatty():
-			print(bcolors.ENDC, end='')
-		SSHClient.close()
-		if not args.sshstop:
-			continue
-		if not args.noop:
-			sys.exit(2)
-		else:
-			print(bcolors.WARNING + "NOOP: skipping to next host due to being a dry run" + bcolors.ENDC)
-			continue
-
-	stdin, stdout, stderr = SSHClient.exec_command('/system resource print')
-
-	for line in stdout:
-		line = line.rstrip('\r\n')
-		if args.verbose and args.verbose >= 3:
-			print('... ' + line)
-		m = MikroTik_regex.match(line)
-		if m:
-			if (m.group(1) == 'version'):
-				version = m.group(2)
-			if (m.group(1) == 'architecture-name'):
-				architecture_name = m.group(2)
-			if (m.group(1) == 'board-name'):
-				board_name = m.group(2)
-			if (m.group(1) == 'bad-blocks'):
-				bad_blocks = m.group(2)
-
-	if args.verbose and args.verbose >= 2:
-		print("\tversion: " + version)
-		print("\tarchitecture-name: " + architecture_name)
-		print("\tboard-name: " + board_name)
-		print("\tbad-blocks: " + bad_blocks)
-
-	if (version == ""):
-		print("Failed to get current RouterOS version. Skipping upgrade.")
-		SSHClient.close()
-		continue
-	else:
-		m = MikroTik_version_regex.match(version)
-		if m:
-			version = m.group(1)
-		CurVersion = packaging.version.parse(version)
-
-	if (architecture_name == ""):
-		print("Failed to get RouterOS architecture-name. Skipping upgrade.")
-		SSHClient.close()
-		continue
-
-	if (board_name == "CHR"):
-		architecture_name = "x86"
-
-	# Check if newer version
-	SSHClient.exec_command('/system package update check-for-updates once')
-	time.sleep(3)
-	stdin, stdout, stderr = SSHClient.exec_command('/system package update print')
-	
-	for line in stdout:
-		line = line.rstrip('\r\n')
-		if args.verbose and args.verbose >= 3:
-			print('... ' + line)
-		m = MikroTik_regex.match(line)
-		if m:
-			if (m.group(1) == 'installed-version'):
-				installed_version = m.group(2)
-			if (m.group(1) == 'latest-version'):
-				latest_version = m.group(2)
-			if (m.group(1) == 'status'):
-				status = m.group(2)
-
-	if args.verbose and args.verbose >= 2:
-		print("\tinstalled-version: " + installed_version)
-		print("\tlatest-version: " + latest_version)
-		print("\tstatus: " + status)
-
-	if installed_version != latest_version:
-		print("RouterOS version from {} to {}".format(installed_version,latest_version))
-
-		stdin, stdout, stderr = SSHClient.exec_command('/system package update install')
-
-		reboot_time = time.time()
-		time.sleep(5)
-
-		host_up = False
-		timeout = time.time() + reboot_timeout
-		while time.time() < timeout:
-			#pingable = os.system("fping -q " + hostname + " 2>/dev/null")
-			responses, no_responses = multi_ping([hostname], timeout=1, retry=1)
-			if not no_responses:
-				host_up = True
-				break
-			if sys.stdout.isatty():
-				print('\r{:.0f} seconds since reboot...'.format(time.time() - reboot_time), end='', flush=True)
-		if sys.stdout.isatty():
-			print('\r', end='')
-			for i in range(0,shutil.get_terminal_size().columns):
-				print(' ', end='')
-			print('\r', end='')
-
-		if host_up:
-			reboot_time = time.time() - reboot_time
-			print('{} is back online after {:.0f} seconds. Checking status'.format(hostname, reboot_time), flush=True)
-			time.sleep(5)	# Wait 5 seconds for the device to fully boot
-
-			version	= ""
-			uptime	= ""
-			CurVersion = ""
-			connected = False
-			retries   = 0
-			while not connected:
-				try:
-					SSHClient.connect(hostname, username=username, password=password, timeout=timeout)
-					connected = True
-					break
-				except paramiko.SSHException as e:
-					if retries > sshretries:
-						break
-					print(bcolors.WARNING + "SSH connection failed with '{}'. Retrying.".format(e) + bcolors.ENDC)
-					retries += 1
-					time.sleep(retries)
-			if not connected:
-				if sys.stdout.isatty():
-					print(bcolors.FAIL, end='')
-				print("ERROR: SSH connection failed. Updates to ALL FURTHER devices cancelled!")
-				if sys.stdout.isatty():
-					print(bcolors.ENDC, end='')
-				SSHClient.close()
-				if not args.noop:
-					sys.exit(2)
-				else:
-					print(bcolors.WARNING + "NOOP: skipping to next host due to being a dry run" + bcolors.ENDC)
-					continue
-
-			stdin, stdout, stderr = SSHClient.exec_command('/system resource print')
-
-			for line in stdout:
-				line = line.rstrip('\r\n')
-				if args.verbose and args.verbose >= 3:
-					print('... ' + line)
-				m = MikroTik_regex.match(line)
-				if m:
-					if (m.group(1) == 'version'):
-						version = m.group(2)
-					if (m.group(1) == 'uptime'):
-						uptime = m.group(2)
-
-			if (version == ""):
-				if sys.stdout.isatty():
-					print(bcolors.FAIL, end='')
-				print("ERROR: Could not confirm RouterOS version. Updates to ALL FURTHER devices cancelled!")
-				if sys.stdout.isatty():
-					print(bcolors.ENDC, end='')
-				SSHClient.close()
-				if not args.noop:
-					sys.exit(2)
-				else:
-					print(bcolors.WARNING + "NOOP: continuing due to being a dry run" + bcolors.ENDC)
-
-			m = MikroTik_version_regex.match(version)
-			if m:
-				version = m.group(1)
-			CurVersion = packaging.version.parse(version)
-			LastVersion = packaging.version.parse(latest_version)
-			if (CurVersion < LastVersion):
-				if sys.stdout.isatty():
-					print(bcolors.FAIL, end='')
-				print("ERROR: Upgrade of {} did not occur, current RouterOS version {}. Updates to ALL FURTHER devices cancelled!".format(hostname,version))
-				if sys.stdout.isatty():
-					print(bcolors.ENDC, end='')
-				SSHClient.close()
-				if not args.noop:
-					sys.exit(2)
-				else:
-					print(bcolors.WARNING + "NOOP: continuing due to being a dry run" + bcolors.ENDC)
-			else:
-				if sys.stdout.isatty():
-					print(bcolors.OKGREEN, end='')
-				print("{} RouterOS successfully upgraded. Version now {}".format(hostname,version))
-				if sys.stdout.isatty():
-					print(bcolors.ENDC, end='')
-
-		else:
-			if sys.stdout.isatty():
-				print(bcolors.FAIL, end='')
-			print("ERROR: {} has NOT come back online within {} seconds. Updates to ALL FURTHER devices cancelled!".format(hostname,reboot_timeout))
-			if sys.stdout.isatty():
-				print(bcolors.ENDC, end='')
-			if not args.noop:
-				sys.exit(2)
-			else:
-				print(bcolors.WARNING + "NOOP: continuing due to being a dry run" + bcolors.ENDC)
-
-	else:
-		print("RouterOS version already {}".format(version))
-
-	SSHClient.close()
-	print()
+    print("\n\n\n{:<16}: {:^12}: {:^12}".format("Hostname", "RouterOS", "Firmware"))
+    for k, v in upgradeStatus.items():
+        print("{:<16}: {:<6} {:>4} : {:<6} {:>4}".format(k, str(v[0]), str(v[1]), str(v[2]), str(v[3])))
